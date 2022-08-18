86f70c811716dba43732fedcdf6c413fc83588bc
suy2on <tnwjdrms@naver.com>
2022-08-18 14:52:07 +0900

    # feat : allowed host 추가

--- secondeyes_server/settings.py
+++ secondeyes_server/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.37.123.193']
+ALLOWED_HOSTS = ['3.37.123.193', 'ec2-3-37-123-193.ap-northeast-2.compute.amazonaws.com']
 
 
 # Application definition
