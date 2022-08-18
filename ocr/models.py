328f06f505c427be8add80675d958a9fcef33f57
suy2on <tnwjdrms@naver.com>
2022-08-18 13:59:39 +0900

    # feat : database 스키마구축

--- ocr/models.py
+++ ocr/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=50)
+    tracks_lat = models.FloatField()
+    tracks_long = models.FloatField()
+
+
+class Exit(models.Model):
+    number = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name="exit")
+
+
